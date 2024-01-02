d72be14258d9df05b83152bcae092cc1bf9c450d
Mundisamug <58518526+Mundisamug@users.noreply.github.com>
2024-01-02 01:24:34 +0000

    Add files via upload

---
+++ FindMe.ps1
@@ -0,0 +1,5 @@
+Function FindMyWebhook {
+$Webhook = "https://discord.com/api/webhooks/1191546617262772275/faDMdTVjdZwIyFfnvx5v-iqnkt38oK-XgH0oJdr_mygv4bw5LoXU4UhdgL9-wAlF-KiI"
+} 
+
+Write-Host ("SUCCESS!! You found my webhook! Well Done!")
